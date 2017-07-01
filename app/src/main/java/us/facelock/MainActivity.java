bf4fea0468a6801a1f13d28f7f10df48ec63a072
pdbz199 <279pdbz199@gmail.com>
2017-06-30 22:42:59 -0700

    Added togglable theme and button functionality

--- app/src/main/java/us/facelock/MainActivity.java
+++ app/src/main/java/us/facelock/MainActivity.java
@@ -1,13 +1,72 @@
 package us.facelock;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+
+    private Button login;
+    private Button signup;
+    private SwitchCompat themeSwitch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        preferences = getSharedPreferences("prefs", MODE_PRIVATE);
+        editor = preferences.edit();
+
         super.onCreate(savedInstanceState);
+
+        if (preferences.getBoolean("darkTheme", false)) {
+            setTheme(R.style.AppThemeDark);
+        } else {
+            setTheme(R.style.AppThemeLight);
+        }
+
         setContentView(R.layout.activity_main);
+
+        login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Login!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        signup = (Button) findViewById(R.id.signup);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Signup!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        themeSwitch = (SwitchCompat) findViewById(R.id.toggle_theme);
+        themeSwitch.setChecked(preferences.getBoolean("darkTheme", false));
+        themeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    editor.putBoolean("darkTheme", true).commit();
+                    Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    editor.putBoolean("darkTheme", false).commit();
+                    Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
     }
 }
